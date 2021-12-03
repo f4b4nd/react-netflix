7a01b6d9d101b713fe814675bb114c6aa90b776b
fabien <fabien.andrian@gmail.com>
2021-12-03 16:21:29 +0100

    header ok

--- netflix/src/app.js
+++ netflix/src/app.js
@@ -1,18 +1,30 @@
 import './app.scss'
-import Home from './pages/home'
+ 
+import { Home, SignIn, SignUp, Browse } from './pages'
+import { HeaderContainer } from './containers/header'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import * as ROUTES from './constants/routes'
 
 export default function App () {
   return (
-    <Router>
+    <>
+    
+
+    <Router><HeaderContainer/>
       <Routes>
 
-        <Route exact path={ROUTES.HOME} element={<Home /> }/>          
+        <Route exact path={ROUTES.HOME} element={<Home/>}/>
+
+        <Route exact path={ROUTES.SIGN_IN} element={<SignIn/>}/>
+
+        <Route exact path={ROUTES.SIGN_UP} element={<SignUp/>}/>
+
+        <Route exact path={ROUTES.BROWSE} element={<Browse/>}/>
       
       </Routes> 
     </Router>
+    </>
   )
 }
 
