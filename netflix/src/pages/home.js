118ad156f2c48ee55acb83b9cc8653dab22a9775
fabien <fabien.andrian@gmail.com>
2021-12-03 16:34:56 +0100

    add feature component

--- netflix/src/pages/home.js
+++ netflix/src/pages/home.js
@@ -3,18 +3,24 @@ import JumbotronContainer from '../containers/jumbotron'
 import FooterContainer from '../containers/footer'
 import { HeaderContainer } from '../containers/header'
 
-import { OptForm } from '../components'
+import { OptForm, Feature } from '../components'
 
 export default function Home () {
   return (
       <>
-        <HeaderContainer>        
-            <OptForm>
-                <OptForm.Input placeholder="Email address" />
-                <OptForm.Button>Try it now</OptForm.Button>
-                <OptForm.Break />
-                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
-            </OptForm>
+        <HeaderContainer>
+            <Feature>
+                <Feature.Title> Unlimited films, TV programms and more. </Feature.Title>
+                <Feature.SubTitle> Watch anywher. Cancel at any time. </Feature.SubTitle>
+            
+                <OptForm>
+                    <OptForm.Input placeholder="Email address" />
+                    <OptForm.Button>Try it now</OptForm.Button>
+                    <OptForm.Break />
+                    <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+                </OptForm>
+                
+            </Feature>
         </HeaderContainer>
 
         <JumbotronContainer />      
