afa78e1ddb503684c808e525969e80dfdbfb5823
fabien <fabien.andrian@gmail.com>
2021-12-14 12:02:35 +0100

    minor

--- netflix/src/lib/firebase.prod.js
+++ netflix/src/lib/firebase.prod.js
@@ -3,16 +3,15 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 
-const firebase = Firebase.initializeApp(
-    {
-        "apiKey": process.env.REACT_APP_API_KEY,
-        "authDomain": process.env.REACT_APP_AUTH_DOMAIN,
-        "projectId": process.env.REACT_APP_PROJECT_ID,
-        "storageBucket": process.env.REACT_APP_STORAGE_BUCKET,
-        "messagingSenderId": process.env.REACT_APP_MESSAGING_SENDER_ID,
-        "appId": process.env.REACT_APP_APP_ID,
-        "measurementId": process.env.REACT_APP_MEASUREMENT_ID
-    })
+const firebase = Firebase.initializeApp({
+    "apiKey": process.env.REACT_APP_API_KEY,
+    "authDomain": process.env.REACT_APP_AUTH_DOMAIN,
+    "projectId": process.env.REACT_APP_PROJECT_ID,
+    "storageBucket": process.env.REACT_APP_STORAGE_BUCKET,
+    "messagingSenderId": process.env.REACT_APP_MESSAGING_SENDER_ID,
+    "appId": process.env.REACT_APP_APP_ID,
+    "measurementId": process.env.REACT_APP_MEASUREMENT_ID
+})
 
 // 1) when seeding the database you'll have to uncomment this!
 /*
